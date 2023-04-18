860619e5d4d7e552fd130be76df4287d905ef74a
GochenRyan <rangaocheng@outlook.com>
2023-04-18 19:56:56 +0800

    Create xmake.lua

---
+++ xmake.lua
@@ -0,0 +1,36 @@
+target("libBX")
+    set_kind("static")
+
+    local BX_DIR = "$(projectdir)/Vendor/bx/"
+
+    add_includedirs(BX_DIR .. "include")
+    add_includedirs(BX_DIR .. "include/bx/inline")
+
+    if is_plat("windows") then 
+        add_defines("__STDC_LIMIT_MACROS"
+                , "__STDC_FORMAT_MACROS"
+                , "__STDC_CONSTANT_MACROS"
+                , "NDEBUG"
+                , "WIN32"
+                , "_WIN32"
+                , "_HAS_EXCEPTIONS=0"
+                , "_HAS_ITERATOR_DEBUGGING=0"
+                , "_ITERATOR_DEBUG_LEVEL=0"
+                , "_SCL_SECURE=0"
+                , "_SECURE_SCL=0"
+                , "_SCL_SECURE_NO_WARNINGS"
+                , "_CRT_SECURE_NO_WARNINGS"
+                , "_CRT_SECURE_NO_DEPRECATE"
+                , "BX_CONFIG_DEBUG")
+
+        add_includedirs(BX_DIR .. "include/compat/msvc")
+        add_includedirs(BX_DIR .. "3rdparty")
+
+        add_files(BX_DIR .. "src/**.cpp|amalgamated.cpp") --|crtnone.cpp
+
+    elseif is_plat("macosx") then
+        add_includedirs(BX_DIR .. "include/compat/osx")
+        add_includedirs(BX_DIR .. "3rdparty")
+
+        add_files(BX_DIR .. "src/**.cpp|amalgamated.cpp")
+    end
